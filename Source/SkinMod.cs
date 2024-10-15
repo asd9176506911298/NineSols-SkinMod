1334a3bcd34bd79a6353add572af144e715d1aec
Yuki.kaco <a0936660023@gmail.com>
2024-10-15 13:17:36 +0800

    Mult Skin Support

--- Source/SkinMod.cs
+++ Source/SkinMod.cs
@@ -4,6 +4,7 @@ using BepInEx.Logging;
 using HarmonyLib;
 using NineSolsAPI;
 using NineSolsAPI.Utils;
+using System.Linq.Expressions;
 using UnityEngine;
 
 namespace SkinMod;
@@ -11,15 +12,23 @@ namespace SkinMod;
 [BepInDependency(NineSolsAPICore.PluginGUID)]
 [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
 public class SkinMod : BaseUnityPlugin {
-    private ConfigEntry<bool> enableSkinConfig = null!;
+    private ConfigEntry<bool> enableSkinConfig = null;
     private ConfigEntry<KeyboardShortcut> enableSkinKeyboardShortcut = null!;
 
+    private ConfigEntry<string> curSkin = null;
+    private ConfigEntry<bool> danceYi = null;
+    private ConfigEntry<bool> jieChuan = null;
+
     private Harmony harmony = null!;
+
+    private string objectPath;
+    private string objectName;
+    private GameObject curSkinObject;
 
     private bool isEnableSkin = false;
 
     private AssetBundle tree;
-    private GameObject danceObject;
+    private GameObject danceYiObject;
     private GameObject jieChuanObject;
 
     private void Awake() {
@@ -31,13 +40,42 @@ public class SkinMod : BaseUnityPlugin {
         enableSkinKeyboardShortcut = Config.Bind("", "Enable Skin Shortcut",
             new KeyboardShortcut(KeyCode.Q, KeyCode.LeftShift), "");
 
+        curSkin = Config.Bind<string>("", "currSkin", "", "");
+        danceYi = Config.Bind<bool>("", "DanceYi", true, "");
+        jieChuan = Config.Bind<bool>("", "JieChuan", false, "");
+
+        danceYi.SettingChanged += DanceYi_SettingChanged;
+        jieChuan.SettingChanged += JieChuan_SettingChanged;
+
         KeybindManager.Add(this, ToggleSkin, () => enableSkinKeyboardShortcut.Value);
 
         Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
 
         tree = AssemblyUtils.GetEmbeddedAssetBundle("SkinMod.Resources.tree");
-        danceObject = tree.LoadAsset<GameObject>("danceRemoveObject");
+        danceYiObject = tree.LoadAsset<GameObject>("danceRemoveObject");
         jieChuanObject = tree.LoadAsset<GameObject>("JieChuan");
+    }
+
+    private void DanceYi_SettingChanged(object sender, System.EventArgs e) {
+        curSkin.Value = "DanceYi";
+        curSkinObject = danceYiObject;
+        objectName = "danceRemoveObject";
+        jieChuan.Value = false;
+        danceYi.Value = false;
+        ProcessVisible(true);
+        isEnableSkin = false;
+        changeObjectPath();
+    }
+
+    private void JieChuan_SettingChanged(object sender, System.EventArgs e) {
+        curSkin.Value = "JieChuan";
+        curSkinObject = jieChuanObject;
+        objectName = "JieChuan";
+        jieChuan.Value = false;
+        danceYi.Value = false;
+        ProcessVisible(true);
+        isEnableSkin = false;
+        changeObjectPath();
     }
 
     void EnableShadow(bool enable) {
@@ -48,48 +86,93 @@ public class SkinMod : BaseUnityPlugin {
         }
     }
 
+    void changeObjectPath() {
+        objectPath = $"GameCore(Clone)/RCG LifeCycle/PPlayer/RotateProxy/SpriteHolder/{objectName}(Clone)";
+    }
+
+    void ProcessVisible(bool enable) {
+        DisableSkin(enable);
+        HideYiSprite(!enable);
+        EnableShadow(enable);
+    }
+
     private void ToggleSkin() {
         ToastManager.Toast("ToggleSkin DDD");
-        Logger.LogInfo("ToggleSkin");
 
-        ToastManager.Toast(GameObject.Find("CameraCore/DummyPlayer/ShadowRoot"));
+        //ToastManager.Toast(GameObject.Find("CameraCore/DummyPlayer/ShadowRoot"));
 
         if (Player.i == null) return;
 
-        bool hasSkin = GameObject.Find("GameCore(Clone)/RCG LifeCycle/PPlayer/RotateProxy/SpriteHolder/JieChuan(Clone)");
+        changeObjectPath();
+
+        bool hasSkin = GameObject.Find(objectPath);
 
         if (hasSkin) 
             {
             if (isEnableSkin)
             {
-                GameObject.Find("GameCore(Clone)/RCG LifeCycle/PPlayer/RotateProxy/SpriteHolder/JieChuan(Clone)").SetActive(false);
-                GameObject.Find("GameCore(Clone)/RCG LifeCycle/PPlayer/RotateProxy/SpriteHolder/PlayerSprite").layer = LayerMask.NameToLayer("Player");
+                ToastManager.Toast("hasSkin isEnableSkin");
+                ProcessVisible(true);
                 isEnableSkin = false;
-                EnableShadow(true);
             } 
             else
             {
-                GameObject.Find("GameCore(Clone)/RCG LifeCycle/PPlayer/RotateProxy/SpriteHolder/JieChuan(Clone)").SetActive(true);
-                GameObject.Find("GameCore(Clone)/RCG LifeCycle/PPlayer/RotateProxy/SpriteHolder/PlayerSprite").layer = LayerMask.NameToLayer("UI");
+                ToastManager.Toast("hasSkin no isEnableSkin");
+                ProcessVisible(false);
                 isEnableSkin = true;
-                EnableShadow(false);
             }
         }
         else
         {
-            Vector3 dancePos = new Vector3(Player.i.transform.position.x, Player.i.transform.position.y + 35, Player.i.transform.position.z);
-            GameObject skin = Instantiate(jieChuanObject, dancePos, Quaternion.identity, GameObject.Find("GameCore(Clone)/RCG LifeCycle/PPlayer/RotateProxy/SpriteHolder").transform);
-            // Yi Dance
-            //skin.transform.localPosition = new Vector3(0f, 11.2001f, 0f);
-            //skin.transform.localScale = new Vector3(0.55f, 0.55f, 0.55f);
-            // JieChuan 
-            skin.transform.localPosition = new Vector3(0.6006f, 11.6006f, 0f);
-            skin.transform.localScale = new Vector3(5f, 5f, 5f);
-
-            GameObject.Find("GameCore(Clone)/RCG LifeCycle/PPlayer/RotateProxy/SpriteHolder/PlayerSprite").layer = LayerMask.NameToLayer("UI");
+            CreateSkin();
+            HideYiSprite(true);
             isEnableSkin = true;
             EnableShadow(false);
         }
+    }
+
+    void DisableSkin(bool isDisable) {
+        GameObject skin = GameObject.Find(objectPath);
+        ToastManager.Toast($"DisableSkin {skin}");
+        if (skin == null) return;
+
+        if (isDisable)
+            skin.SetActive(false);
+        else
+            skin.SetActive(true);
+    }
+
+    void HideYiSprite(bool isHide) {
+        GameObject PlayerSprite = GameObject.Find("GameCore(Clone)/RCG LifeCycle/PPlayer/RotateProxy/SpriteHolder/PlayerSprite");
+
+        ToastManager.Toast($"{PlayerSprite} {isHide}");
+
+        if (PlayerSprite == null) return;
+
+        if (isHide) {
+            PlayerSprite.layer = LayerMask.NameToLayer("UI");
+        } else {
+            PlayerSprite.layer = LayerMask.NameToLayer("Player");
+        }
+    }
+
+    void CreateSkin() {
+        Vector3 skinPos = new Vector3(Player.i.transform.position.x, Player.i.transform.position.y + 35, Player.i.transform.position.z);
+        GameObject skinClone = Instantiate(curSkinObject, skinPos, Quaternion.identity, GameObject.Find("GameCore(Clone)/RCG LifeCycle/PPlayer/RotateProxy/SpriteHolder").transform);
+
+        if (curSkin.Value == "DanceYi") {
+            // Yi Dance
+            skinClone.transform.localPosition = new Vector3(0f, 11.2001f, 0f);
+            skinClone.transform.localScale = new Vector3(0.55f, 0.55f, 0.55f);
+        } else if (curSkin.Value == "JieChuan") {
+            // JieChuan 
+            skinClone.transform.localPosition = new Vector3(0.6006f, 11.6006f, 0f);
+            skinClone.transform.localScale = new Vector3(5f, 5f, 5f);
+        }
+        
+        GameObject.Find("GameCore(Clone)/RCG LifeCycle/PPlayer/RotateProxy/SpriteHolder/PlayerSprite").layer = LayerMask.NameToLayer("UI");
+        isEnableSkin = true;
+        EnableShadow(false);
     }
 
     private void OnDestroy() {  
